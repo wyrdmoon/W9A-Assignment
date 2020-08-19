f9e56b506bbbf8eef21142dbec10981fea82bc44
wyrdmoon <criscorcoran@gmail.com>
2020-08-18 21:06:38 -0600

    assignment w9a

---
+++ app.js
@@ -0,0 +1,16 @@
+var age = 18;
+var is_subscribed = false;
+
+if ((age >= 18) && (is_subscribed == false)){
+    console.log("The User is under 18 and is not subscribed");
+}
+else if ((age =< 18) && (is_subscribed == false)){
+    console.log("The User is older than 18 and not subscribed");
+}
+else ((age =< 18)) && (is_subscribed == true)) {
+    console.log ("the User is older than 18 and is_subscribed");
+}
+if ((age >= 18) && (is_subscribed == false)) {
+    console.log("the User is under than 18 and is_subscribed");
+}
+
