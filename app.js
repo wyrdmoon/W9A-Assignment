a323e835e3da93541fc7b626e7874b80b8525fab
wyrdmoon <criscorcoran@gmail.com>
2020-08-20 20:38:22 -0600

    set up arrays and loop on w9a assignment making it w9b assignment

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-var age = 18;
-var is_subscribed = true;
+var age = [ 18, 17, 16, 15, 14, 13 ]
+var is_subscribed = [ true, false, true, false]
 
-if ((age < 18) && (is_subscribed == false)){
+console.log(userAge = 18);
+console.log(is_subscribed);
+
+for (index = 0; index < 6; index++) {
+
+if ((age [index] <= 18) && (is_subscribed [index] == false)){
     console.log("The User is under 18 and is not subscribed");
 }
-else if ((age >= 18) && (is_subscribed == false)){
+else if ((age [index] >= 18) && (is_subscribed [index] == false)){
     console.log("The User is older than 18 and not subscribed");
 }
-else if ((age >= 18) && (is_subscribed == true)) {
+else if ((age [index]>= 18) && (is_subscribed [index] == true)) {
     console.log ("the User is older than 18 and is_subscribed");
 }
-else if ((age < 18) && (is_subscribed == true)) {
+else if ((age [index] <= 18) && (is_subscribed [index] == true)) {
     console.log("the User is under than 18 and is_subscribed");
 }
 else {
     console.log ("error");
 }
-
+console.log(index);
+}
